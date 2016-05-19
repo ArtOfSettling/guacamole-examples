6ae1c852ba6456af70a6b51b4dbe8b4257dfde82
unknown <terrymagnusdrever@wellfired.com>
2016-05-19 17:47:09 +0100

    TextFieldTest.

---
+++ TextFieldExample/Editor/TextFieldTestApplication.cs
@@ -0,0 +1,23 @@
+﻿using WellFired.Guacamole;
+
+namespace WellFired.TextFieldTest.Editor
+{
+	public class TextFieldTestApplication
+	{
+		[UnityEditor.MenuItem("Window/guacamole/Test/TextField")]
+		static void OpenWindow()
+		{
+			var application = new Guacamole.Unity.Editor.Application();
+
+			var context = new Guacamole.Unity.Editor.ApplicationInitializationContext
+			{
+				MainContent = typeof(TextFieldTestWindow),
+				UIRect = new UIRect(50, 50, 600, 200),
+				MinSize = new UISize(260, 30),
+				Title = "TestFieldTest"
+			};
+
+			application.Launch(context);
+		}
+	}
+}
---
+++ TextFieldExample/Editor/TextFieldTestWindow.cs
@@ -0,0 +1,23 @@
+﻿using WellFired.Guacamole;
+
+namespace WellFired.TextFieldTest.Editor
+{
+	public class TextFieldTestWindow : Window
+	{
+		public TextFieldTestWindow()
+		{
+			BackgroundColor = UIColor.Grey;
+			Padding = new UIPadding(5);
+			
+			var textEntry = new TextEntry
+			{
+				HorizontalLayout = LayoutOptions.Expand,
+				BackgroundColor = UIColor.White,
+				Text = "Text",
+				Label = "Example"
+			};
+
+			Content = textEntry;
+		}
+	}
+}
