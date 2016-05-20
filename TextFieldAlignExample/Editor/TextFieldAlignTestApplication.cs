a66b579afd1a4e880a24d21fd29a60e4bfc098ee
unknown <terrymagnusdrever@wellfired.com>
2016-05-20 10:00:28 +0100

    Added a textfield font align text test.

---
+++ TextFieldAlignExample/Editor/TextFieldAlignTestApplication.cs
@@ -0,0 +1,23 @@
+﻿using WellFired.Guacamole;
+
+namespace WellFired.TextFieldTest.Editor
+{
+	public class TextFieldAlignTestApplication
+	{
+		[UnityEditor.MenuItem("Window/guacamole/Test/TextFieldAlign")]
+		static void OpenWindow()
+		{
+			var application = new Guacamole.Unity.Editor.Application();
+
+			var context = new Guacamole.Unity.Editor.ApplicationInitializationContext
+			{
+				MainContent = typeof(TextFieldAlignTestWindow),
+				UIRect = new UIRect(50, 50, 600, 200),
+				MinSize = new UISize(260, 230),
+				Title = "TestFieldAlignTest"
+			};
+
+			application.Launch(context);
+		}
+	}
+}
--- TextFieldExample/Editor/TextFieldTestWindow.cs
+++ TextFieldExample/Editor/TextFieldTestWindow.cs
@@ -8,36 +8,13 @@ namespace WellFired.TextFieldTest.Editor
 		{
 			Padding = new UIPadding(5);
 			
-			var textEntryLeft = new TextEntry
+			var textEntryStartStart = new TextEntry
 			{
 				BackgroundColor = UIColor.White,
-				Text = "LeftAlign"
+				Text = "Test"
 			};
 
-			var textEntryCenter = new TextEntry
-			{
-				HorizontalTextAlign = UITextAlign.Midle,
-				BackgroundColor = UIColor.White,
-				Text = "CenterAlign"
-			};
-
-			var textEntryRight = new TextEntry
-			{
-				HorizontalTextAlign = UITextAlign.End,
-				BackgroundColor = UIColor.White,
-				Text = "RightAlign"
-			};
-
-			Content = new AdjacentLayout
-			{
-				Spacing = 5,
-				Children =
-				{
-					textEntryLeft,
-					textEntryCenter,
-					textEntryRight
-				}
-			};
+			Content = textEntryStartStart;
 		}
 	}
 }
