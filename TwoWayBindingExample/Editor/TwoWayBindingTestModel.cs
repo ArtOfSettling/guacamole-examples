fc3279adc26b78ff00ae8be1231f0019c396022b
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-22 17:13:38 +0100

    TwoWayBindingExample.

---
+++ TwoWayBindingExample/Editor/TwoWayBindingTestModel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using WellFired.Guacamole.Databinding.Unity.Editor;
+
+namespace WellFired.Guacamole.Examples.TwoWayBinding
+{
+	public class TwoWayBindingTestModel : ObservableScriptableObject
+	{
+		[SerializeField] private string _boundText;
+		public string BoundText
+		{
+			get { return _boundText; }
+			set { SetProperty(ref _boundText, value, "BoundText"); }
+		}
+	}
+}
---
+++ TwoWayBindingExample/Editor/TwoWayBindingTestWindow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using WellFired.Guacamole.Databinding;
+using WellFired.Test1.Editor;
+
+namespace WellFired.Guacamole.Examples.TwoWayBinding
+{
+	public class TwoWayBindingTestWindow : Window
+	{
+		public TwoWayBindingTestWindow()
+		{
+			Padding = new UIPadding(5);
+			
+			var boundTextEntry = new TextEntry
+			{
+				BackgroundColor = UIColor.White,
+				Text = "SampleText"
+			};
+
+			Content = boundTextEntry;
+
+			const string assetPath = "Assets/guacamole-examples/TwoWayBindingExample/Editor/WindowData.asset";
+			var windowData = AssetDatabase.LoadAssetAtPath<TwoWayBindingTestModel>(assetPath);
+			if (windowData == null)
+			{
+				windowData = ScriptableObject.CreateInstance<TwoWayBindingTestModel>();
+				AssetDatabase.CreateAsset(windowData, assetPath);
+			}
+
+			BindingContext = new TwoWayBindingTestViewModel { Model = windowData };
+
+			boundTextEntry.Bind(TextEntry.TextProperty, "BoundText", BindingMode.TwoWay);
+		}
+	}
+}
