9f9a69cf6f7284800f3b39009cff76d81bf2ccf9
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-25 15:57:45 +0100

    Simple test binding UI elements together.

---
+++ UIBindingExample/Editor/UIBindingTestWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WellFired.Guacamole.Examples.UIBinding
+{
+	public class UIBindingTestWindow : Window
+	{
+		public UIBindingTestWindow()
+		{
+			Padding = new UIPadding(5);
+			
+			var destinationElement = new TextEntry
+			{
+				BackgroundColor = UIColor.White
+			};
+
+			var sourceElement = new Slider
+			{
+				BackgroundColor = UIColor.White
+			};
+
+			Content = new AdjacentLayout
+			{
+				Children =
+				{
+					destinationElement,
+					sourceElement
+				}
+			};
+
+			destinationElement.BindingContext = sourceElement;
+
+			destinationElement.Bind(TextEntry.TextProperty, "Value");
+		}
+	}
+}
